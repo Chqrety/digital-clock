f6fadc7a1a0529da025ee6e95ebf712a8a185246
Chqrety <fikriaverus23@gmail.com>
2025-04-29 10:35:16 +0700

    ⬆️Update: upgrade responsive view for all time

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,23 +4,27 @@ import Image from "next/image";
 import React, { useEffect, useState } from "react";
 
 export default function Home() {
-    const [time, setTime] = useState("");
-    useEffect(() => {
-        const updateClock = () => {
-            const now = new Date();
-            const hours = String(now.getHours()).padStart(2, "0");
-            const minutes = String(now.getMinutes()).padStart(2, "0");
-            const seconds = String(now.getSeconds()).padStart(2, "0");
-            setTime(`${hours}:${minutes}:${seconds}`);
-        };
+  const [time, setTime] = useState([]);
+  useEffect(() => {
+    const updateClock = () => {
+      const now = new Date();
+      const hours = String(now.getHours()).padStart(2, "0");
+      const minutes = String(now.getMinutes()).padStart(2, "0");
+      const seconds = String(now.getSeconds()).padStart(2, "0");
+      setTime({ hours: hours, minutes: minutes, seconds: seconds });
+    };
 
-        updateClock(); // Panggil sekali saat mount
-        const interval = setInterval(updateClock, 1000);
-        return () => clearInterval(interval);
-    }, []);
-    return (
-        <div className="flex w-full justify-center items-center h-screen">
-            <span className="text-8xl md:text-[10rem] lg:text-[20rem]">{time}</span>
-        </div>
-    );
+    updateClock(); // Panggil sekali saat mount
+    const interval = setInterval(updateClock, 1000);
+    return () => clearInterval(interval);
+  }, []);
+  return (
+    <div className="flex items-center justify-center w-full h-screen text-8xl md:text-[10rem] lg:text-[20rem]">
+      <span className="w-[2ch] flex justify-end">{time.hours}</span>
+      <span>:</span>
+      <span className="w-[2ch] flex justify-center">{time.minutes}</span>
+      <span>:</span>
+      <span className="w-[2ch] flex justify-start">{time.seconds}</span>
+    </div>
+  );
 }
